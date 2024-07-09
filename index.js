20cc750b6508abe880b8eea4f31b31ff735e727f
T-Rajeev30 <113722532+T-Rajeev30@users.noreply.github.com>
2024-07-10 02:20:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+var typed = new Typed(".typed-text",{
+    strings:["Full-Stack Developer.","Ui-Ux Designer.","3-D Artist."],
+    typeSpeed :40,
+    backSpeed:10,
+    loop:true
+})
+
+const spans = document.querySelectorAll('.progress-bar span');
+
+spans.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+});
+
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
